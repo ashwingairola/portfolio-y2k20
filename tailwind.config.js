06a609e357812db64a4c9f5cb1a5044554752029
Ashwin Gairola <ashwingairola999@gmail.com>
2020-02-27 19:00:42 +0530

    Tailwind Theming done.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	theme: {
+		colors: {
+			primary: 'var(--color-primary)',
+			secondary: 'var(--color-secondary)'
+		},
+		textColor: {
+			primary: 'var(--color-text-primary)',
+			secondary: 'var(--color-text-secondary)'
+		},
+		backgroundColor: {
+			primary: 'var(--color-bg-primary)',
+			secondary: 'var(--color-bg-secondary)'
+		}
+	}
+};
