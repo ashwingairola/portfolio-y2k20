c5078ef9c6523a62586cfc301a3766e077faab43
Ashwin Gairola <ashwingairola999@gmail.com>
2020-03-10 20:58:09 +0530

    Linting changes.

--- src/app/components/sidenav-item/sidenav-item.component.ts
+++ src/app/components/sidenav-item/sidenav-item.component.ts
@@ -13,24 +13,14 @@ import {
 	styleUrls: ['./sidenav-item.component.scss'],
 	animations: [
 		trigger('showHide', [
-			state(
-				'*',
-				style({
-                    opacity: 1,
-				})
-			),
-			state(
-				'void',
-				style({
-					opacity: 0,
-				})
-			),
-			transition('* <=> void', [animate('0.1s')])
-		]),
+			state('*', style({ opacity: 1 })),
+			state('void', style({ opacity: 0 })),
+			transition('* <=> void', [animate('0.2s')])
+		])
 	]
 })
 export class SidenavItemComponent implements OnInit {
-    @Input() icon!: string;
+	@Input() icon!: string;
 	hovering: boolean;
 
 	constructor() {
