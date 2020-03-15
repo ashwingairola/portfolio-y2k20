3deabea67f432d663e99c709ef1e6f20fd3ed645
Ashwin Gairola <ashwingairola999@gmail.com>
2020-03-15 18:12:18 +0530

    Resume download link created.

--- src/app/routes/home/components/banner/banner.component.ts
+++ src/app/routes/home/components/banner/banner.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { environment as env } from '@ag-env';
 
 @Component({
 	selector: 'ag-banner',
 	templateUrl: './banner.component.html',
 	styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent implements OnInit {
-	constructor() {}
+export class BannerComponent {
+	resumeUrl: string;
+	resumeFilename: string;
 
-	ngOnInit(): void {}
-
-	downloadResume() {
-		console.log('DOWNLOADING');
+	constructor() {
+		this.resumeUrl = env.resume.url;
+		this.resumeFilename = env.resume.filename;
 	}
 }
