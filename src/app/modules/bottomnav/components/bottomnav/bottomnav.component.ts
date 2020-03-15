f105b570d9326ae5629775d1b02017c5d5bfec21
Ashwin Gairola <ashwingairola999@gmail.com>
2020-03-15 16:50:59 +0530

    BottomNavComponent done.

--- src/app/modules/bottomnav/components/bottomnav/bottomnav.component.ts
+++ src/app/modules/bottomnav/components/bottomnav/bottomnav.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	AfterViewInit,
+	Input,
+	ViewChild,
+	ElementRef,
+	Renderer2
+} from '@angular/core';
+import { ThemeColor } from '@ag-modules';
 
 @Component({
-  selector: 'ag-bottomnav',
-  templateUrl: './bottomnav.component.html',
-  styleUrls: ['./bottomnav.component.scss']
+	selector: 'ag-bottomnav',
+	templateUrl: './bottomnav.component.html',
+	styleUrls: ['./bottomnav.component.scss']
 })
-export class BottomnavComponent implements OnInit {
+export class BottomnavComponent implements AfterViewInit {
+	@Input() color!: ThemeColor;
+	@ViewChild('bottomnavContainer') bottomnavContainer!: ElementRef<
+		HTMLDivElement
+	>;
 
-  constructor() { }
+	constructor(private renderer: Renderer2) {}
 
-  ngOnInit(): void {
-  }
+	ngAfterViewInit(): void {
+		const container = this.bottomnavContainer.nativeElement;
+		let bgClass: string;
+		switch (this.color) {
+			case 'primary':
+				bgClass = 'bg-app-primary';
+				break;
 
+			case 'secondary':
+				bgClass = 'bg-app-secondary';
+				break;
+
+			case 'warn':
+				bgClass = 'bg-warn';
+				break;
+
+			default:
+				bgClass = 'bg-navbar';
+		}
+
+		this.renderer.addClass(container, bgClass);
+	}
 }
