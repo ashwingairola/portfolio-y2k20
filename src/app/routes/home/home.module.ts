f1ff1e39302d7b8b48ed5ff45bca2bb15efb426c
Ashwin Gairola <ashwingairola999@gmail.com>
2020-03-15 11:18:31 +0530

    Re-added ButtonModule.

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { IconModule } from '@ag-modules';
+import { IconModule, ButtonModule } from '@ag-modules';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
@@ -9,6 +9,6 @@ import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
 	declarations: [HomeComponent, BannerComponent],
-	imports: [CommonModule, HomeRoutingModule, IconModule]
+	imports: [CommonModule, HomeRoutingModule, IconModule, ButtonModule]
 })
 export class HomeModule {}
