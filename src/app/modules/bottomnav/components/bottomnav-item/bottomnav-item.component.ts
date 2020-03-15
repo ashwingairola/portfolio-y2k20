670bacb868df85318fd0d59d177a83750d7f7d57
Ashwin Gairola <ashwingairola999@gmail.com>
2020-03-15 16:51:21 +0530

    BottomnavItemComponent done.

--- src/app/modules/bottomnav/components/bottomnav-item/bottomnav-item.component.ts
+++ src/app/modules/bottomnav/components/bottomnav-item/bottomnav-item.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { ClickableComponent } from 'src/app/components/clickable/clickable.component';
+import {
+	trigger,
+	state,
+	style,
+	transition,
+	animate
+} from '@angular/animations';
 
 @Component({
-  selector: 'ag-bottomnav-item',
-  templateUrl: './bottomnav-item.component.html',
-  styleUrls: ['./bottomnav-item.component.scss']
+	selector: 'ag-bottomnav-item',
+	templateUrl: './bottomnav-item.component.html',
+	styleUrls: ['./bottomnav-item.component.scss'],
+	animations: [
+		trigger('showOnHover', [
+			state('*', style({ opacity: 0 })),
+			state('hover', style({ opacity: 0.5 })),
+			transition('* <=> hover', [animate('0.2s')])
+		]),
+		trigger('onActive', [
+			state('*', style({ opacity: 0 })),
+			state('active', style({ opacity: 0.7 })),
+			transition('* <=> active', [animate('0.2s')])
+		])
+	]
 })
-export class BottomnavItemComponent implements OnInit {
+export class BottomnavItemComponent extends ClickableComponent {
+	constructor(elementRef: ElementRef) {
+		super(elementRef);
+	}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
