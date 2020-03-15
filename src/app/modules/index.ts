69660fa12164eef0391f1730c271500a37a08a9c
Ashwin Gairola <ashwingairola999@gmail.com>
2020-03-15 16:48:45 +0530

    ThemeColor added to exports.

--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -1,4 +1,8 @@
+// Modules
 export { SidenavModule } from './sidenav/sidenav.module';
 export { BottomnavModule } from './bottomnav/bottomnav.module';
 export { ButtonModule } from './button/button.module';
 export { IconModule } from './icon/icon.module';
+
+// Models
+export * from './models/color.model';
