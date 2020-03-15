d2bb564d5ed7610422c0ec0e0beeaf639b9543e6
Ashwin Gairola <ashwingairola999@gmail.com>
2020-03-15 11:15:33 +0530

    IconModule import moved to aliased path.

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { IconModule } from '@ag-modules';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { BannerComponent } from './components/banner/banner.component';
-import { IconModule } from 'src/app/modules/icon/icon.module';
-
 
 @NgModule({
-  declarations: [HomeComponent, BannerComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    IconModule
-  ]
+	declarations: [HomeComponent, BannerComponent],
+	imports: [CommonModule, HomeRoutingModule, IconModule]
 })
-export class HomeModule { }
+export class HomeModule {}
