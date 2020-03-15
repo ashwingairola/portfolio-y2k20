bb174de2385b9d212a65626b496f57229732f047
Ashwin Gairola <ashwingairola999@gmail.com>
2020-03-15 18:12:32 +0530

    Old resume download code removed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { environment as env } from '@ag-env';
 
 @Component({
 	selector: 'ag-root',
@@ -7,11 +6,5 @@ import { environment as env } from '@ag-env';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	resumeUrl: string;
-	resumeFilename: string;
-
-	constructor() {
-		this.resumeUrl = env.resume.url;
-		this.resumeFilename = env.resume.filename;
-	}
+	constructor() {}
 }
