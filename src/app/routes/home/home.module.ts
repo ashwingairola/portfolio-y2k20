914d3a07b1814dca20d67bd4d6b57869c0f47803
Ashwin Gairola <ashwingairola999@gmail.com>
2020-03-22 00:58:18 +0530

    Added MyWorkComponent.

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -6,9 +6,10 @@ import { IconModule, ButtonModule } from '@ag-modules';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { MyWorkComponent } from './components/my-work/my-work.component';
 
 @NgModule({
-	declarations: [HomeComponent, BannerComponent],
+	declarations: [HomeComponent, BannerComponent, MyWorkComponent],
 	imports: [CommonModule, HomeRoutingModule, IconModule, ButtonModule]
 })
 export class HomeModule {}
