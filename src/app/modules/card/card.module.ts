56d764c9db660ca09cf19f8f6644ea8d6dfe4ad4
Ashwin Gairola <ashwingairola999@gmail.com>
2020-03-29 20:12:48 +0530

    Created Card header and footer.

--- src/app/modules/card/card.module.ts
+++ src/app/modules/card/card.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
+import { CardHeaderComponent } from './components/card-header/card-header.component';
+import { CardFooterComponent } from './components/card-footer/card-footer.component';
 
 
 
 @NgModule({
-  declarations: [CardComponent],
+  declarations: [CardComponent, CardHeaderComponent, CardFooterComponent],
   imports: [
     CommonModule
   ],
-  exports: [CardComponent]
+  exports: [CardComponent, CardHeaderComponent, CardFooterComponent]
 })
 export class CardModule { }
