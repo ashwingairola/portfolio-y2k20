cc6f34e30a6336e653d73eb401e67c64102b7ab4
Ashwin Gairola <ashwingairola999@gmail.com>
2020-04-01 02:25:52 +0530

    Added IconModule in imports.

--- src/app/modules/slider/slider.module.ts
+++ src/app/modules/slider/slider.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ButtonModule } from '../button/button.module';
+import { IconModule } from '../icon/icon.module';
 
 import { SliderComponent } from './components/slider/slider.component';
 import { SliderItemDirective } from './directives/slider-item.directive';
 
 @NgModule({
 	declarations: [SliderComponent, SliderItemDirective],
-	imports: [CommonModule, ButtonModule],
+	imports: [CommonModule, ButtonModule, IconModule],
 	exports: [SliderComponent, SliderItemDirective]
 })
 export class SliderModule {}
