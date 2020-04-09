4dfde5a5449de9c5a9d98097f87037d48c4e3d92
Ashwin Gairola <ashwingairola999@gmail.com>
2020-04-09 20:25:27 +0530

    BtnGithubComponent created.

---
+++ src/app/routes/home/components/btn-github/btn-github.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+	selector: 'ag-btn-github',
+	templateUrl: './btn-github.component.html',
+	styleUrls: ['./btn-github.component.scss'],
+})
+export class BtnGithubComponent implements OnInit {
+	@Input() githubRepo!: string;
+	githubUrl = 'https://github.com';
+
+	constructor() {}
+
+	ngOnInit(): void {
+		this.githubUrl += `/${this.githubRepo}`;
+	}
+}
