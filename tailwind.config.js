99eef3cbee72edec921db4c9cf7c25b5cae91866
Ashwin Gairola <ashwingairola999@gmail.com>
2020-04-19 13:45:46 +0530

    Added new screens: xxl, fullHD and 4k.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 module.exports = {
 	theme: {
+		extend: {
+			screens: {
+				xxl: '1440px',
+				fullHD: '1920px',
+				'4k': '2560px',
+			},
+		},
 		textColor: {
 			primary: 'var(--color-text-primary)',
 			secondary: 'var(--color-text-secondary)',
 			'app-primary': 'var(--color-primary)',
 			'app-secondary': 'var(--color-secondary)',
-			navbar: 'var(--color-text-navbar)'
+			navbar: 'var(--color-text-navbar)',
 		},
 		backgroundColor: {
 			primary: 'var(--color-primary)',
 			secondary: 'var(--color-secondary)',
 			'app-primary': 'var(--color-bg-primary)',
 			'app-secondary': 'var(--color-bg-secondary)',
-			navbar: 'var(--color-bg-navbar)'
-		}
-	}
+			navbar: 'var(--color-bg-navbar)',
+		},
+	},
 };
