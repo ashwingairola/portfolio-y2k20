f189eda0bf75fa8e201936a88214373cd0612752
Ashwin <ashwingairola999@gmail.com>
2020-08-29 12:49:19 +0530

    Created HyperlinkModule.

---
+++ src/app/modules/hyperlink/directives/external-link.directive.ts
@@ -0,0 +1,35 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ThemeColor } from '../../models/color.model';
+
+@Directive({
+	selector: 'a[agExternalLink]'
+})
+export class ExternalLinkDirective implements OnInit {
+	@Input() color: ThemeColor = 'none';
+
+	constructor(private elementRef: ElementRef<HTMLAnchorElement>) {}
+
+	ngOnInit() {
+		this.setColor();
+		this.setTargetToExternal();
+	}
+
+	private setColor() {
+		const el = this.elementRef.nativeElement;
+
+		switch (this.color) {
+			case 'primary':
+				el.classList.add('text-app-primary');
+				break;
+
+			case 'secondary':
+				el.classList.add('text-app-secondary');
+				break;
+		}
+	}
+
+	private setTargetToExternal() {
+		const el = this.elementRef.nativeElement;
+		el.setAttribute('target', '_blank');
+	}
+}
---
+++ src/app/modules/hyperlink/hyperlink.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExternalLinkDirective } from './directives/external-link.directive';
+
+@NgModule({
+	declarations: [ExternalLinkDirective],
+	imports: [CommonModule],
+	exports: [ExternalLinkDirective]
+})
+export class HyperlinkModule {}
