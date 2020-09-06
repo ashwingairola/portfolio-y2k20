dc7258e7e72d4249cd81f2c880089b64cb62f9de
Ashwin <ashwingairola999@gmail.com>
2020-09-06 12:29:23 +0530

    Using BreakpointObserver to listen to phone and desktop screens.

--- src/app/routes/home/components/banner/banner.component.ts
+++ src/app/routes/home/components/banner/banner.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { environment as env } from '@ag-env';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+
+import { environment as env } from '@ag-env';
+
+const BREAKPOINTS = {
+	tablet: '(min-width: 640px)',
+	desktop: '(min-width: 1280px)'
+};
 
 @Component({
 	selector: 'ag-banner',
@@ -12,25 +18,44 @@ import { takeUntil } from 'rxjs/operators';
 export class BannerComponent implements OnInit, OnDestroy {
 	resumeUrl: string;
 	resumeFilename: string;
-	showLargeBanner: boolean;
+	githubUrl: string;
+	linkedInUrl: string;
+	angelListUrl: string;
+	showLargeHeroSection: boolean;
+	showHeroImage: boolean;
 	unsubscribeSubject: Subject<void>;
 
 	constructor(private breakpointObserver: BreakpointObserver) {
 		this.resumeUrl = env.resume.url;
 		this.resumeFilename = env.resume.filename;
-		this.showLargeBanner = false;
+		this.githubUrl = env.githubUrl;
+		this.linkedInUrl = env.linkedInUrl;
+		this.angelListUrl = env.angelListUrl;
+		this.showLargeHeroSection = false;
+		this.showHeroImage = false;
 		this.unsubscribeSubject = new Subject();
 	}
 
 	ngOnInit() {
 		this.breakpointObserver
-			.observe(['(min-width: 640px)'])
+			.observe([BREAKPOINTS.tablet, BREAKPOINTS.desktop])
 			.pipe(takeUntil(this.unsubscribeSubject))
 			.subscribe(breakpointState => {
-				if (breakpointState.matches) {
-					this.showLargeBanner = true;
+				console.log(breakpointState);
+				const breakpoints = breakpointState.breakpoints;
+
+				// Show or hide the large hero section.
+				if (breakpoints[BREAKPOINTS.tablet]) {
+					this.showLargeHeroSection = true;
 				} else {
-					this.showLargeBanner = false;
+					this.showLargeHeroSection = false;
+				}
+
+				// Show or hide the hero image.
+				if (breakpoints[BREAKPOINTS.desktop]) {
+					this.showHeroImage = true;
+				} else {
+					this.showHeroImage = false;
 				}
 			});
 	}
