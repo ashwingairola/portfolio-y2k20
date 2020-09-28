2496c7ac5343b5768c9e84922e1f8d69dbc757dd
Ashwin <ashwingairola999@gmail.com>
2020-09-28 12:19:15 +0530

    [SliderComponent] added some commented code for extra slider settings.

--- src/app/modules/slider/components/slider/slider.component.ts
+++ src/app/modules/slider/components/slider/slider.component.ts
@@ -1,13 +1,12 @@
 import {
 	Component,
-	OnInit,
 	ViewChild,
 	ElementRef,
 	AfterViewInit,
 	ContentChildren,
 	QueryList,
 	AfterContentInit,
-	Renderer2,
+	Renderer2
 } from '@angular/core';
 import Glide from '@glidejs/glide';
 import { SliderItemDirective } from '../../directives/slider-item.directive';
@@ -15,10 +14,9 @@ import { SliderItemDirective } from '../../directives/slider-item.directive';
 @Component({
 	selector: 'ag-slider',
 	templateUrl: './slider.component.html',
-	styleUrls: ['./slider.component.scss'],
+	styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent
-	implements OnInit, AfterViewInit, AfterContentInit {
+export class SliderComponent implements AfterViewInit, AfterContentInit {
 	@ViewChild('glide', { static: true }) glide!: ElementRef<HTMLDivElement>;
 	@ContentChildren(SliderItemDirective) sliderItems!: QueryList<
 		SliderItemDirective
@@ -26,37 +24,37 @@ export class SliderComponent
 
 	constructor(private renderer: Renderer2) {}
 
-	ngOnInit(): void {}
-
 	ngAfterViewInit(): void {
 		const glideObj = new Glide(this.glide.nativeElement, {
 			perView: 4,
+			// rewind: false,
+			// bound: true,
 			breakpoints: {
 				640: {
-					perView: 1.5,
+					perView: 1.5
 				},
 				768: {
-					perView: 2.5,
+					perView: 2.5
 				},
 				1024: {
-					perView: 3,
+					perView: 3
 				},
 				1280: {
-					perView: 4,
+					perView: 4
 				},
 				1440: {
-					perView: 5,
+					perView: 5
 				},
 				1920: {
-					perView: 4,
+					perView: 4
 				},
 				2560: {
-					perView: 8,
+					perView: 8
 				},
 				3840: {
-					perView: 12,
-				},
-			},
+					perView: 12
+				}
+			}
 		});
 
 		glideObj.mount();
