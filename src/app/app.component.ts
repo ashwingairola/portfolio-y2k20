756c874fb099518b0821f746950564519243b691
Ashwin <ashwingairola999@gmail.com>
2020-09-28 14:02:07 +0530

    Added logic to display loader while lazy-loading.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+	RouteConfigLoadEnd,
+	RouteConfigLoadStart,
+	Router
+} from '@angular/router';
 
 @Component({
 	selector: 'ag-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-	constructor() {}
+export class AppComponent implements OnInit {
+	showLoader = false;
+
+	constructor(private router: Router) {}
+
+	ngOnInit(): void {
+		this.router.events.subscribe(event => {
+			if (event instanceof RouteConfigLoadStart) {
+				this.showLoader = true;
+			} else if (event instanceof RouteConfigLoadEnd) {
+				this.showLoader = false;
+			}
+		});
+	}
 }
