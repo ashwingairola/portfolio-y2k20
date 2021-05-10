afef469f6bf74f85dbb27ab478b689fe817747c8
Ashwin <ashwingairola999@gmail.com>
2021-05-10 19:09:57 +0530

    [feature/skills] Fetching and displaying skills by category.

--- src/app/routes/skills/components/skills/skills.component.ts
+++ src/app/routes/skills/components/skills/skills.component.ts
@@ -9,13 +9,31 @@ import { Observable } from 'rxjs';
 	styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+	programmingLanguages$?: Observable<APIResponse<Skill[]>>;
 	frontendSkills$?: Observable<APIResponse<Skill[]>>;
+	backendSkills$?: Observable<APIResponse<Skill[]>>;
+	dbSkills$?: Observable<APIResponse<Skill[]>>;
+	testingSkills$?: Observable<APIResponse<Skill[]>>;
 
 	constructor(private skillApiService: SkillApiService) {}
 
 	ngOnInit(): void {
+		this.programmingLanguages$ = this.skillApiService.getSkillsByType(
+			ESkillType.LANGUAGES
+		);
+
 		this.frontendSkills$ = this.skillApiService.getSkillsByType(
 			ESkillType.FRONTEND
+		);
+
+		this.backendSkills$ = this.skillApiService.getSkillsByType(
+			ESkillType.BACKEND
+		);
+
+		this.dbSkills$ = this.skillApiService.getSkillsByType(ESkillType.DATABASE);
+
+		this.testingSkills$ = this.skillApiService.getSkillsByType(
+			ESkillType.TESTING
 		);
 	}
 }
